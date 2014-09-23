48fce3f89dac6dc0beb6ea48798cd6cea8fe3bc6
dwking <tjwizking@gmail.com>
2014-09-23 14:48:15 +0000

    GuessGameTester 1st push

---
+++ GuessGameTester.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class GuessGameTester
+{
+
+	public static void main(String args[])
+	{
+		//get input from user
+		Scanner ggso = new Scanner(System.in);
+		String gword  = so.nextLine();
+
+		GuessGame ggo = new GuessGame(gword);
+
+		Boolean isw = isTheWord(gword);
+	}
+}
