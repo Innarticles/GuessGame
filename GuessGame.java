0fb0381af6b743e0927baafe20e60ff5836579ed
Innarticles <innocentudeh@gmail.com>
2014-09-23 17:01:46 -0700

    Full work with things working

--- GuessGame.java
+++ GuessGame.java
@@ -1,16 +1,49 @@
+import java.util.Random;
+import java.util.Scanner;
 public class GuessGame  {
 	private int count;
-	private String cWord;
-	private String[] cities = {"Lagos", "Accra",  "London", "Kumasi", "Jamiaca", "New York"};
+	private int chances = 10;
+	//private String cWord;
+	private String[] cities = {"Lagos", "Accra"}; // "London", "Kumasi", "Jamiaca", "New York"};
+
+	Random rn = new Random();
+	int rdWord = rn.nextInt(2);
+	String cWord = cities[rdWord];
+	Scanner scan = new Scanner(System.in);
 
 	public GuessGame(String word)
 	{
 		cWord = word;
 	}
 	
-	public boolean isTheWord (String w)
+	public GuessGame()
 	{
-		return cWord.equals(w);
+		
+	}
+
+	public boolean isTheWord (String w)
+	{	
+		int i = 0;
+		boolean r =false;
+		while (i < chances)
+		{
+			if (cWord.equals(w))
+				{
+					r = true;
+					System.out.println("Aha! I told you can be lucky");
+					break;
+				}
+			else
+				{	System.out.println("You have " + (chances - i) + " trials left");
+					System.out.println(" ");
+					System.out.print("Wrong! Try again :");
+					w = scan.nextLine();
+					r = false;
+				}
+			i++;
+		}
+		return r;
+		
 	}
 	public boolean isGameOver()
 	{
--- GuessGameTester.java
+++ GuessGameTester.java
@@ -5,13 +5,21 @@ public class GuessGameTester
 	public static void main(String args[])
 	{
 		//get input from user
+		System.out.println("Welcome to aYoInno Mind game!");
+		System.out.println("Simply think of a city in the world. You might just be lucky");
+		System.out.println("");
+		System.out.print("Guess a city: ");
 		Scanner ggso = new Scanner(System.in);
 		String gword  = ggso.nextLine();
 
-		GuessGame ggo = new GuessGame(gword);
+
+		GuessGame ggo = new GuessGame();
 
 		Boolean isw = ggo.isTheWord(gword);
 
-		 System.out.println(isw);
+		if (isw == false)
+		{
+			System.out.println("Hey! - I thought you are smart");
+		}
 	}
 }
