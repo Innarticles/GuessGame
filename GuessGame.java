b0f38522b8dc8deaf7da22a61d04de5a1299cb9d
Innarticles <innocentudeh@gmail.com>
2014-09-23 17:27:20 -0700

    Perfect one

--- GuessGame.java
+++ GuessGame.java
@@ -1,80 +1,54 @@
 import java.util.Random;
 import java.util.Scanner;
-public class GuessGame  {
-	private int count;
-<<<<<<< HEAD
-	private int chances = 10;
-	//private String cWord;
-	private String[] cities = {"Lagos", "Accra"}; // "London", "Kumasi", "Jamiaca", "New York"};
-
-	Random rn = new Random();
-	int rdWord = rn.nextInt(2);
-	String cWord = cities[rdWord];
-	Scanner scan = new Scanner(System.in);
-=======
-	private String cWord;
-
-	private String[] cities = {"Lagos", "Accra",  "London", "Kumasi", "Jamiaca", "New York"};
->>>>>>> 2f377692525d8c57d731e985a86bd0ed3d130369
-
-	public GuessGame(String word)
-	{
-		cWord = word;
-	}
-	
-	public GuessGame()
-	{
-<<<<<<< HEAD
-		
-	}
-
-	public boolean isTheWord (String w)
-	{	
-		int i = 0;
-		boolean r =false;
-		while (i < chances)
+public class GuessGame {
+		private int count;
+		private int chances = 10;
+		//private String cWord;
+		private String[] cities = {"Lagos", "Accra"}; // "London", "Kumasi", "Jamiaca", "New York"};
+		Random rn = new Random();
+		int rdWord = rn.nextInt(2);
+		String cWord = cities[rdWord];
+		Scanner scan = new Scanner(System.in);
+		public GuessGame(String word)
 		{
-			if (cWord.equals(w))
+			cWord = word;
+		}
+		public GuessGame()
+		{
+		}
+
+		public boolean isTheWord (String w)
+		{	
+			int i = 0;
+			boolean r =false;
+			while (i < chances)
 				{
-					r = true;
-					System.out.println("Aha! I told you can be lucky");
-					break;
+					if (cWord.equals(w))
+						{
+							r = true;
+							System.out.println("Aha! I told you can be lucky");
+							break;
+						}
+					else
+						{	System.out.println("You have " + (chances - i) + " trials left");
+							System.out.println(" ");
+							System.out.print("Wrong! Try again :");
+							w = scan.nextLine();
+							r = false;
+						}
+							i++;
+					}
+					return r;
 				}
-			else
-				{	System.out.println("You have " + (chances - i) + " trials left");
-					System.out.println(" ");
-					System.out.print("Wrong! Try again :");
-					w = scan.nextLine();
-					r = false;
-				}
-			i++;
-		}
-		return r;
-		
-=======
-		int i =0;
-		boolean right= false;
-		while( i < cities.length)
-		{
-			if(w.equals(cities[i]))
+		public boolean isGameOver()
 			{
-				right = true;
-				break;
+			if (count>10)
+			{
+				return false;
 			}
 			else
 			{
-				right = false;
+				return true;
 			}
-			i++;
 		}
-		return right;
->>>>>>> 2f377692525d8c57d731e985a86bd0ed3d130369
-	}
-	public boolean isGameOver()
-	{
-		if (count>10)
-			{return false;}
-		else
-			{return true;}
-	}
 }
--- GuessGameTester.java
+++ GuessGameTester.java
@@ -1,25 +1,21 @@
 import java.util.Scanner;
 public class GuessGameTester
 {
-
-	public static void main(String args[])
-	{
-		//get input from user
+public static void main(String args[])
+{
+//get input from user
 		System.out.println("Welcome to aYoInno Mind game!");
 		System.out.println("Simply think of a city in the world. You might just be lucky");
 		System.out.println("");
 		System.out.print("Guess a city: ");
+
 		Scanner ggso = new Scanner(System.in);
-		String gword  = ggso.nextLine();
-
-
+		String gword = ggso.nextLine();
 		GuessGame ggo = new GuessGame();
-
 		Boolean isw = ggo.isTheWord(gword);
-
 		if (isw == false)
-		{
-			System.out.println("Hey! - I thought you are smart");
+			{
+				System.out.println("Hey! - I thought you are smart");
+			}
 		}
-	}
 }
